13744e7f0181927f9864f20b9e5bb5211d6702a9
TereshinaEVa <tereshina.eva@gmail.com>
2024-08-24 13:45:34 +0300

    Многопроцессное программирование

---
+++ module_10_5.py
@@ -0,0 +1,29 @@
+import multiprocessing
+from datetime import datetime
+
+def read_info(name):
+    all_data = []
+    with open(name, 'r') as f:
+        string_file = f.readlines()
+        for i in string_file:
+            all_data.append(i)
+
+"""start_time = datetime.now()
+
+filenames = [f'./Files/file {number}.txt' for number in range(1,5)]
+for name in filenames:
+    read_info(name)
+end_time = datetime.now()
+print(end_time - start_time)"""
+
+if __name__ == '__main__':
+    with multiprocessing.Pool(processes=2) as pool:
+        filenames = [f'./Files/file {number}.txt' for number in range(1, 5)]
+        start_time = datetime.now()
+        pool.map(read_info, filenames)
+    end_time = datetime.now()
+
+    print(end_time - start_time)
+
+#lines: 0:00:25.380893
+#multiprocessing: 0:00:11.626563 (у меня комп слабенький, поэтому только при использовании двух процессов имеет смысл)
